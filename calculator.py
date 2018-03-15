ea9f67e5d24a5539708836816e256b4a64028aea
josanFFMC <josan.preijde@futurefacts.nl>
2018-03-15 11:15:07 +0100

    fixed print

--- calculator.py
+++ calculator.py
@@ -29,16 +29,16 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 if choice == 1:
-   print(num1, "+", num2, "=", add(num1,num2))
+   print('{0} + {1} = {2}'.format(num1, num2, add(num1, num2)))
 
 elif choice == 2:
-   print(num1,"-",num2,"=", subtract(num1,num2))
+   print('{0} - {1} = {2}'.format(num1,num2, subtract(num1,num2)))
 
 elif choice == 3:
-   print(num1,"*",num2,"=", multiply(num1,num2))
+   print('{0} * {1} = {2}'.format(num1,num2, multiply(num1,num2)))
 
 elif choice == 4:
-   print(num1,"/",num2,"=", divide(num1,num2))
+   print('{0} / {1} = {2}'.format(num1,num2, divide(num1,num2)))
 else:
    print("Invalid input")
 
